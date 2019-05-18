63248a10bc7f7d6042850b0836861722598cf77a
Tom W <twilliams1711@gmail.com>
2019-05-18 23:02:33 +1000

    Added empty projects

---
+++ src/SevenWest.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SevenWest.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/SevenWest.Core/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace SevenWest.Core.Model
+{
+    public class User
+    {
+        public string First { get; set; }
+
+        public string Last { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+
+        public int Id { get; set; }
+    }
+}
